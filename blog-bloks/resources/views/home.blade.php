3cef96d02312e00535bc6589d9bcea048817fe85
fbabris <fbabris2@gmail.com>
2023-10-05 06:23:55 +0300

    first commit

---
+++ blog-bloks/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+    <div class="main">
+        <h1>Home page</h1>
+        <p>This is the home page</p>
+    </div>
+</body>
+</html>
