1a0c1c189357dc8f5847112416e4156074277e6a
fbabris <fbabris2@gmail.com>
2023-10-07 11:12:29 +0300

    added validation of user input

--- blog-bloks/resources/views/about.blade.php
+++ blog-bloks/resources/views/about.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-</head>
-<body>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="about">About</a></li>
-    </ul>
-    <div class="main">
-        <h1>About page</h1>
-        <p>This is the about page.</p>
-    </div>
-</body>
-</html>
+@extends('layout')
+
+@section('title', 'About us')
+
+@section ('content')
+    <h1>About page</h1>
+    <p>This is the about page</p>
+@endsection
--- blog-bloks/resources/views/home.blade.php
+++ blog-bloks/resources/views/home.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-</head>
-<body>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="about">About</a></li>
-    </ul>
-    <div class="main">
-        <h1>Home page</h1>
-        <p>This is the home page</p>
-    </div>
-</body>
-</html>
+@extends('layout')
+
+@section('title', 'home')
+
+@section ('content')
+    <h1>Home page</h1>
+    <p>This is the home page</p>
+@endsection
