8a70ee2d2e70c86023e187282094fde388a554d8
Filips Babris <fbabris2@gmail.com>
2023-10-10 11:39:46 +0300

    basic testing

--- blog-bloks/database/migrations/2023_10_09_200620_add_user_id_to_posts_table.php
+++ blog-bloks/database/migrations/2023_10_09_200620_add_user_id_to_posts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             $table->foreignId('user_id')
+                 ->nullable()
                 ->contrained()
                 ->onDelete('cascade');
         });
